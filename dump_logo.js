68737f0931bd7fa7bc965640869e27287a99b666
kamarudheen mv <kamaru916@gmail.com>
2026-04-14 17:52:03 +0530

    security: remove dangerous and redundant standalone scripts for production

--- dump_logo.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = 'c:\\Users\\kamar\\OneDrive\\Desktop\\Loud IMC projects\\WaterFilterProject\\leewaa-ecom\\storefront\\public\\images\\Leewa_logo_web.png';
-const image = fs.readFileSync(path);
-const base64 = 'data:image/png;base64,' + image.toString('base64');
-fs.writeFileSync('logo_base64.txt', base64);
-console.log('Success: Full base64 string saved to logo_base64.txt');
--- get_logo_base64.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const path = 'c:\\Users\\kamar\\OneDrive\\Desktop\\Loud IMC projects\\WaterFilterProject\\leewaa-ecom\\storefront\\public\\images\\Leewa_logo_web.png';
-const image = fs.readFileSync(path);
-console.log('data:image/png;base64,' + image.toString('base64'));
